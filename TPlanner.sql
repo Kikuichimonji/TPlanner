002c4bdd1f51ee16d713bf73de686d335cd2abdd
Roess Thomas <thomas_roess@hotmail.fr>
2021-12-13 22:50:22 +0100

    update DB export

--- TPlanner.sql
+++ TPlanner.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- Hôte:                         localhost
--- Version du serveur:           5.7.24 - MySQL Community Server (GPL)
--- SE du serveur:                Win64
--- HeidiSQL Version:             11.3.0.6295
+-- Host:                         localhost
+-- Server version:               5.7.24 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             10.2.0.5599
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -10,31 +10,35 @@
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Listage de la structure de la base pour tplanner
+-- Dumping database structure for tplanner
 CREATE DATABASE IF NOT EXISTS `tplanner` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `tplanner`;
 
--- Listage de la structure de la table tplanner. board
+-- Dumping structure for table tplanner.board
 CREATE TABLE IF NOT EXISTS `board` (
-  `id_board` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `label` varchar(50) NOT NULL,
   `id_environnement` int(11) DEFAULT NULL,
   `id_user` int(11) NOT NULL,
-  PRIMARY KEY (`id_board`),
+  PRIMARY KEY (`id`),
   KEY `id_environnement` (`id_environnement`),
   KEY `id_user` (`id_user`),
   CONSTRAINT `board_ibfk_1` FOREIGN KEY (`id_environnement`) REFERENCES `environnement` (`id_environnement`),
-  CONSTRAINT `board_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  CONSTRAINT `board_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Dumping data for table tplanner.board: ~2 rows (approximately)
+/*!40000 ALTER TABLE `board` DISABLE KEYS */;
+INSERT INTO `board` (`id`, `label`, `id_environnement`, `id_user`) VALUES
+	(1, 'Board 1', NULL, 1),
+	(2, 'Board 2', NULL, 1);
+/*!40000 ALTER TABLE `board` ENABLE KEYS */;
 
--- Listage de la structure de la table tplanner. card
+-- Dumping structure for table tplanner.card
 CREATE TABLE IF NOT EXISTS `card` (
-  `id_card` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NOT NULL,
   `description` text,
   `positions` smallint(6) NOT NULL,
@@ -43,14 +47,21 @@ CREATE TABLE IF NOT EXISTS `card` (
   `deadline` datetime DEFAULT NULL,
   `isArchived` tinyint(1) NOT NULL,
   `id_list` int(11) NOT NULL,
-  PRIMARY KEY (`id_card`),
+  PRIMARY KEY (`id`),
   KEY `id_list` (`id_list`),
-  CONSTRAINT `card_ibfk_1` FOREIGN KEY (`id_list`) REFERENCES `list` (`id_list`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  CONSTRAINT `card_ibfk_1` FOREIGN KEY (`id_list`) REFERENCES `lists` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Dumping data for table tplanner.card: ~4 rows (approximately)
+/*!40000 ALTER TABLE `card` DISABLE KEYS */;
+INSERT INTO `card` (`id`, `title`, `description`, `positions`, `color`, `picture`, `deadline`, `isArchived`, `id_list`) VALUES
+	(1, 'Card1', NULL, 1, NULL, NULL, NULL, 0, 1),
+	(2, 'Card2', NULL, 2, NULL, NULL, NULL, 0, 1),
+	(3, 'Card3', NULL, 1, NULL, NULL, NULL, 0, 2),
+	(4, 'Card4', NULL, 2, NULL, NULL, NULL, 0, 2);
+/*!40000 ALTER TABLE `card` ENABLE KEYS */;
 
--- Listage de la structure de la table tplanner. comment
+-- Dumping structure for table tplanner.comment
 CREATE TABLE IF NOT EXISTS `comment` (
   `id_comment` int(11) NOT NULL AUTO_INCREMENT,
   `text` text NOT NULL,
@@ -58,35 +69,44 @@ CREATE TABLE IF NOT EXISTS `comment` (
   `id_card` int(11) NOT NULL,
   PRIMARY KEY (`id_comment`),
   KEY `id_card` (`id_card`),
-  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`id_card`) REFERENCES `card` (`id_card`)
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`id_card`) REFERENCES `card` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Dumping data for table tplanner.comment: ~0 rows (approximately)
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
 
--- Listage de la structure de la table tplanner. environnement
+-- Dumping structure for table tplanner.environnement
 CREATE TABLE IF NOT EXISTS `environnement` (
   `id_environnement` int(11) NOT NULL AUTO_INCREMENT,
   `label` varchar(50) NOT NULL,
   PRIMARY KEY (`id_environnement`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Dumping data for table tplanner.environnement: ~0 rows (approximately)
+/*!40000 ALTER TABLE `environnement` DISABLE KEYS */;
+/*!40000 ALTER TABLE `environnement` ENABLE KEYS */;
 
--- Listage de la structure de la table tplanner. list
-CREATE TABLE IF NOT EXISTS `list` (
-  `id_list` int(11) NOT NULL AUTO_INCREMENT,
+-- Dumping structure for table tplanner.lists
+CREATE TABLE IF NOT EXISTS `lists` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `label` varchar(50) NOT NULL,
   `listPosition` tinyint(4) NOT NULL,
   `isArchived` tinyint(1) NOT NULL,
   `id_board` int(11) NOT NULL,
-  PRIMARY KEY (`id_list`),
+  PRIMARY KEY (`id`),
   KEY `id_board` (`id_board`),
-  CONSTRAINT `list_ibfk_1` FOREIGN KEY (`id_board`) REFERENCES `board` (`id_board`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  CONSTRAINT `lists_ibfk_1` FOREIGN KEY (`id_board`) REFERENCES `board` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Dumping data for table tplanner.lists: ~2 rows (approximately)
+/*!40000 ALTER TABLE `lists` DISABLE KEYS */;
+INSERT INTO `lists` (`id`, `label`, `listPosition`, `isArchived`, `id_board`) VALUES
+	(1, '1st List', 0, 0, 1),
+	(2, '2nd List', 1, 0, 1);
+/*!40000 ALTER TABLE `lists` ENABLE KEYS */;
 
--- Listage de la structure de la table tplanner. tag
+-- Dumping structure for table tplanner.tag
 CREATE TABLE IF NOT EXISTS `tag` (
   `id_tag` int(11) NOT NULL AUTO_INCREMENT,
   `label` varchar(50) DEFAULT NULL,
@@ -94,49 +114,61 @@ CREATE TABLE IF NOT EXISTS `tag` (
   `id_card` int(11) NOT NULL,
   PRIMARY KEY (`id_tag`),
   KEY `id_card` (`id_card`),
-  CONSTRAINT `tag_ibfk_1` FOREIGN KEY (`id_card`) REFERENCES `card` (`id_card`)
+  CONSTRAINT `tag_ibfk_1` FOREIGN KEY (`id_card`) REFERENCES `card` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Dumping data for table tplanner.tag: ~0 rows (approximately)
+/*!40000 ALTER TABLE `tag` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tag` ENABLE KEYS */;
 
--- Listage de la structure de la table tplanner. users
+-- Dumping structure for table tplanner.users
 CREATE TABLE IF NOT EXISTS `users` (
-  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
   `mail` varchar(50) NOT NULL,
   `role` text,
   `dateCreation` date NOT NULL,
-  PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Dumping data for table tplanner.users: ~0 rows (approximately)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `username`, `password`, `mail`, `role`, `dateCreation`) VALUES
+	(1, 'pseudo', 'password', 'mail@mail.mail', NULL, '2021-12-13');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
--- Listage de la structure de la table tplanner. usersboard
+-- Dumping structure for table tplanner.usersboard
 CREATE TABLE IF NOT EXISTS `usersboard` (
   `id_user` int(11) NOT NULL,
   `id_board` int(11) NOT NULL,
   PRIMARY KEY (`id_user`,`id_board`),
   KEY `id_board` (`id_board`),
-  CONSTRAINT `usersboard_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`),
-  CONSTRAINT `usersboard_ibfk_2` FOREIGN KEY (`id_board`) REFERENCES `board` (`id_board`)
+  CONSTRAINT `usersboard_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`),
+  CONSTRAINT `usersboard_ibfk_2` FOREIGN KEY (`id_board`) REFERENCES `board` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Dumping data for table tplanner.usersboard: ~2 rows (approximately)
+/*!40000 ALTER TABLE `usersboard` DISABLE KEYS */;
+INSERT INTO `usersboard` (`id_user`, `id_board`) VALUES
+	(1, 1),
+	(1, 2);
+/*!40000 ALTER TABLE `usersboard` ENABLE KEYS */;
 
--- Listage de la structure de la table tplanner. userscard
+-- Dumping structure for table tplanner.userscard
 CREATE TABLE IF NOT EXISTS `userscard` (
   `id_user` int(11) NOT NULL,
   `id_card` int(11) NOT NULL,
   PRIMARY KEY (`id_user`,`id_card`),
   KEY `id_card` (`id_card`),
-  CONSTRAINT `userscard_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`),
-  CONSTRAINT `userscard_ibfk_2` FOREIGN KEY (`id_card`) REFERENCES `card` (`id_card`)
+  CONSTRAINT `userscard_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`),
+  CONSTRAINT `userscard_ibfk_2` FOREIGN KEY (`id_card`) REFERENCES `card` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Dumping data for table tplanner.userscard: ~0 rows (approximately)
+/*!40000 ALTER TABLE `userscard` DISABLE KEYS */;
+/*!40000 ALTER TABLE `userscard` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
