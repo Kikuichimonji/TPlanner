f8741058f256c68cdd8723440548f3523dd89089
Roess Thomas <thomas_roess@hotmail.fr>
2022-01-03 15:05:40 +0100

    new db names

--- TPlanner.sql
+++ TPlanner.sql
@@ -13,11 +13,11 @@
 
 
 -- Dumping database structure for tplanner
-CREATE DATABASE IF NOT EXISTS `tplanner` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE IF NOT EXISTS `tplanner` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `tplanner`;
 
--- Dumping structure for table tplanner.board
-CREATE TABLE IF NOT EXISTS `board` (
+-- Dumping structure for table tplanner.boards
+CREATE TABLE IF NOT EXISTS `boards` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `label` varchar(50) NOT NULL,
   `id_environnement` int(11) DEFAULT NULL,
@@ -25,20 +25,20 @@ CREATE TABLE IF NOT EXISTS `board` (
   PRIMARY KEY (`id`),
   KEY `id_environnement` (`id_environnement`),
   KEY `id_user` (`id_user`),
-  CONSTRAINT `board_ibfk_1` FOREIGN KEY (`id_environnement`) REFERENCES `environnement` (`id_environnement`),
-  CONSTRAINT `board_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`)
+  CONSTRAINT `boards_ibfk_1` FOREIGN KEY (`id_environnement`) REFERENCES `environnements` (`id_environnement`),
+  CONSTRAINT `boards_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--- Dumping data for table tplanner.board: ~2 rows (approximately)
-/*!40000 ALTER TABLE `board` DISABLE KEYS */;
-INSERT INTO `board` (`id`, `label`, `id_environnement`, `id_user`) VALUES
-	(1, 'Board 1', NULL, 1),
-	(2, 'Board 2', NULL, 1),
+-- Dumping data for table tplanner.boards: ~2 rows (approximately)
+/*!40000 ALTER TABLE `boards` DISABLE KEYS */;
+INSERT INTO `boards` (`id`, `label`, `id_environnement`, `id_user`) VALUES
+	(1, 'My first board', NULL, 1),
+	(2, 'Board deux', NULL, 1),
 	(3, 'Board 3', NULL, 1);
-/*!40000 ALTER TABLE `board` ENABLE KEYS */;
+/*!40000 ALTER TABLE `boards` ENABLE KEYS */;
 
--- Dumping structure for table tplanner.card
-CREATE TABLE IF NOT EXISTS `card` (
+-- Dumping structure for table tplanner.cards
+CREATE TABLE IF NOT EXISTS `cards` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NOT NULL,
   `description` text,
@@ -50,46 +50,51 @@ CREATE TABLE IF NOT EXISTS `card` (
   `id_list` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_list` (`id_list`),
-  CONSTRAINT `card_ibfk_1` FOREIGN KEY (`id_list`) REFERENCES `lists` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=76 DEFAULT CHARSET=latin1;
+  CONSTRAINT `cards_ibfk_1` FOREIGN KEY (`id_list`) REFERENCES `lists` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=98 DEFAULT CHARSET=latin1;
 
--- Dumping data for table tplanner.card: ~7 rows (approximately)
-/*!40000 ALTER TABLE `card` DISABLE KEYS */;
-INSERT INTO `card` (`id`, `title`, `description`, `positions`, `color`, `picture`, `deadline`, `isArchived`, `id_list`) VALUES
-	(47, 'Card 1', NULL, 0, NULL, NULL, NULL, 0, 2),
-	(48, 'Card 2', NULL, 1, NULL, NULL, NULL, 0, 2),
+-- Dumping data for table tplanner.cards: ~12 rows (approximately)
+/*!40000 ALTER TABLE `cards` DISABLE KEYS */;
+INSERT INTO `cards` (`id`, `title`, `description`, `positions`, `color`, `picture`, `deadline`, `isArchived`, `id_list`) VALUES
+	(48, 'Card 2', NULL, 0, NULL, NULL, NULL, 0, 3),
 	(49, 'CArd 3', NULL, 0, NULL, NULL, NULL, 0, 1),
-	(50, 'Card 4', NULL, 0, NULL, NULL, NULL, 0, 3),
-	(72, 'card', NULL, 0, NULL, NULL, NULL, 0, 11),
-	(74, 'Card 5', NULL, 2, NULL, NULL, NULL, 0, 2),
-	(75, 'Card 6', NULL, 1, NULL, NULL, NULL, 0, 1);
-/*!40000 ALTER TABLE `card` ENABLE KEYS */;
+	(50, 'Card 4', NULL, 2, NULL, NULL, NULL, 0, 2),
+	(74, 'Card 5', NULL, 0, NULL, NULL, NULL, 0, 2),
+	(75, 'Card 6', NULL, 1, NULL, NULL, NULL, 0, 1),
+	(76, 'Card', NULL, 1, NULL, NULL, NULL, 0, 3),
+	(77, 'pouet', 'Description', 0, NULL, NULL, NULL, 0, 11),
+	(78, 'a card', NULL, 1, NULL, NULL, NULL, 0, 2),
+	(84, 'Card 1', '>1\n>2\n>3', 3, NULL, NULL, NULL, 0, 3),
+	(93, 'zerzer', 'aazeeraazeeraazeeraazeeraazeeraazeeraazeeraazeeraazeeraazeeraaazeeraazeeraazeeraazeeraazeeraazeeraazeeraazeeraazeeraazeeraaazeeraazeeraazeeraazeeraazeeraazeeraazeeraazeeraazeeraazeeraaazeeraazeeraazeeraazeeraazeeraazeeraazeeraa', 0, NULL, NULL, NULL, 0, 16),
+	(96, 'alert();', NULL, 2, NULL, NULL, NULL, 0, 3),
+	(97, 'adaqzer', 'Cette description', 1, NULL, NULL, NULL, 0, 16);
+/*!40000 ALTER TABLE `cards` ENABLE KEYS */;
 
--- Dumping structure for table tplanner.comment
-CREATE TABLE IF NOT EXISTS `comment` (
+-- Dumping structure for table tplanner.comments
+CREATE TABLE IF NOT EXISTS `comments` (
   `id_comment` int(11) NOT NULL AUTO_INCREMENT,
   `text` text NOT NULL,
   `dateCreation` datetime NOT NULL,
   `id_card` int(11) NOT NULL,
   PRIMARY KEY (`id_comment`),
   KEY `id_card` (`id_card`),
-  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`id_card`) REFERENCES `card` (`id`)
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`id_card`) REFERENCES `cards` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table tplanner.comment: ~0 rows (approximately)
-/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
-/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+-- Dumping data for table tplanner.comments: ~0 rows (approximately)
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
 
--- Dumping structure for table tplanner.environnement
-CREATE TABLE IF NOT EXISTS `environnement` (
+-- Dumping structure for table tplanner.environnements
+CREATE TABLE IF NOT EXISTS `environnements` (
   `id_environnement` int(11) NOT NULL AUTO_INCREMENT,
   `label` varchar(50) NOT NULL,
   PRIMARY KEY (`id_environnement`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table tplanner.environnement: ~0 rows (approximately)
-/*!40000 ALTER TABLE `environnement` DISABLE KEYS */;
-/*!40000 ALTER TABLE `environnement` ENABLE KEYS */;
+-- Dumping data for table tplanner.environnements: ~0 rows (approximately)
+/*!40000 ALTER TABLE `environnements` DISABLE KEYS */;
+/*!40000 ALTER TABLE `environnements` ENABLE KEYS */;
 
 -- Dumping structure for table tplanner.lastchange
 CREATE TABLE IF NOT EXISTS `lastchange` (
@@ -101,7 +106,7 @@ CREATE TABLE IF NOT EXISTS `lastchange` (
 -- Dumping data for table tplanner.lastchange: ~0 rows (approximately)
 /*!40000 ALTER TABLE `lastchange` DISABLE KEYS */;
 INSERT INTO `lastchange` (`id`, `lastChange`) VALUES
-	(1, '2021-12-20 08:08:03');
+	(1, '2022-01-02 16:15:41');
 /*!40000 ALTER TABLE `lastchange` ENABLE KEYS */;
 
 -- Dumping structure for table tplanner.lists
@@ -113,16 +118,17 @@ CREATE TABLE IF NOT EXISTS `lists` (
   `id_board` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_board` (`id_board`),
-  CONSTRAINT `lists_ibfk_1` FOREIGN KEY (`id_board`) REFERENCES `board` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+  CONSTRAINT `lists_ibfk_1` FOREIGN KEY (`id_board`) REFERENCES `boards` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table tplanner.lists: ~5 rows (approximately)
 /*!40000 ALTER TABLE `lists` DISABLE KEYS */;
 INSERT INTO `lists` (`id`, `label`, `listPosition`, `isArchived`, `id_board`) VALUES
 	(1, '1st List', 0, 0, 1),
 	(2, '2nd List', 1, 0, 1),
-	(3, '3rd list', 2, 0, 1),
-	(11, 'sdfsdf', 1, 0, 2);
+	(3, '3rd liste', 2, 0, 1),
+	(11, 'sdfsdf', 2, 0, 2),
+	(16, 'azeazeazezzzzzsdfsdfsdfsdsdfsdfsdfsdfsdfsdfsdfrrrr', 3, 0, 1);
 /*!40000 ALTER TABLE `lists` ENABLE KEYS */;
 
 -- Dumping structure for table tplanner.tag
@@ -133,7 +139,7 @@ CREATE TABLE IF NOT EXISTS `tag` (
   `id_card` int(11) NOT NULL,
   PRIMARY KEY (`id_tag`),
   KEY `id_card` (`id_card`),
-  CONSTRAINT `tag_ibfk_1` FOREIGN KEY (`id_card`) REFERENCES `card` (`id`)
+  CONSTRAINT `tag_ibfk_1` FOREIGN KEY (`id_card`) REFERENCES `cards` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table tplanner.tag: ~0 rows (approximately)
@@ -150,12 +156,13 @@ CREATE TABLE IF NOT EXISTS `users` (
   `dateCreation` date NOT NULL,
   `color` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table tplanner.users: ~1 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `username`, `password`, `mail`, `role`, `dateCreation`, `color`) VALUES
-	(1, 'pseudo', 'password', 'mail@mail.mail', NULL, '2021-12-13', '#3688db');
+	(1, 'pseudo', '$argon2i$v=19$m=65536,t=4,p=1$SWhPRW5kM2llMlowbVgyYw$ULnIb46zLfocBQ/gms9v7hFUBkYDa1yFi2RSax6bMqg', 'mail@mail.mail', NULL, '2021-12-13', '#3688db'),
+	(2, 'pseudo2', '$argon2i$v=19$m=65536,t=4,p=1$SWhPRW5kM2llMlowbVgyYw$ULnIb46zLfocBQ/gms9v7hFUBkYDa1yFi2RSax6bMqg', 'mail2@mail2.mail2', NULL, '2022-01-02', '#12a09c');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 -- Dumping structure for table tplanner.usersboard
@@ -165,7 +172,7 @@ CREATE TABLE IF NOT EXISTS `usersboard` (
   PRIMARY KEY (`id_user`,`id_board`),
   KEY `id_board` (`id_board`),
   CONSTRAINT `usersboard_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`),
-  CONSTRAINT `usersboard_ibfk_2` FOREIGN KEY (`id_board`) REFERENCES `board` (`id`)
+  CONSTRAINT `usersboard_ibfk_2` FOREIGN KEY (`id_board`) REFERENCES `boards` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table tplanner.usersboard: ~2 rows (approximately)
@@ -176,19 +183,19 @@ INSERT INTO `usersboard` (`id_user`, `id_board`) VALUES
 	(1, 3);
 /*!40000 ALTER TABLE `usersboard` ENABLE KEYS */;
 
--- Dumping structure for table tplanner.userscard
-CREATE TABLE IF NOT EXISTS `userscard` (
+-- Dumping structure for table tplanner.userscards
+CREATE TABLE IF NOT EXISTS `userscards` (
   `id_user` int(11) NOT NULL,
   `id_card` int(11) NOT NULL,
   PRIMARY KEY (`id_user`,`id_card`),
   KEY `id_card` (`id_card`),
-  CONSTRAINT `userscard_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`),
-  CONSTRAINT `userscard_ibfk_2` FOREIGN KEY (`id_card`) REFERENCES `card` (`id`)
+  CONSTRAINT `userscards_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`),
+  CONSTRAINT `userscards_ibfk_2` FOREIGN KEY (`id_card`) REFERENCES `cards` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table tplanner.userscard: ~0 rows (approximately)
-/*!40000 ALTER TABLE `userscard` DISABLE KEYS */;
-/*!40000 ALTER TABLE `userscard` ENABLE KEYS */;
+-- Dumping data for table tplanner.userscards: ~0 rows (approximately)
+/*!40000 ALTER TABLE `userscards` DISABLE KEYS */;
+/*!40000 ALTER TABLE `userscards` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
