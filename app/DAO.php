fe8937ab764580d2de6512f1b33e4a263b1851d8
Roess Thomas <thomas_roess@hotmail.fr>
2022-01-03 08:02:22 +0100

    APP comments

--- app/DAO.php
+++ app/DAO.php
@@ -1,7 +1,7 @@
 <?php 
     namespace App;
     
-    abstract class DAO{
+    abstract class DAO{ //Why is it called DAO again? i don't remember, i'm just used to do it
 
         protected static $link;
         const DB_HOST = "localhost";
@@ -9,8 +9,8 @@
         const DB_USER = "root";
         const DB_PASS = "";
 
-        public static function connect(){
-            try{
+        public static function connect(){ //We create the connection to the DB
+            try{//If the link does not exist already we create a new one
                 self::$link = self::$link ?? (new \PDO("mysql:host=".self::DB_HOST.";port=3306;".
                                                         " dbname=".self::DB_NAME,
                                                         self::DB_USER,
@@ -19,18 +19,17 @@
                                                         \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
                                                         \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
                                                     ])); 
-                //var_dump(self::$link);die();
                 return self::$link;
             }
             catch(\PDOException $e) {
-                echo APP_ENV === "dev"
+                echo APP_ENV === "dev" //Showing different errors depending on the environnement 
                         ?  $e->getMessage()
                         :  "An error occured";
                 die();
             }
             
         }
-        public static function disconnect(){
+        public static function disconnect(){ //we destroy the link to the DB to disconnect
             self::$link = null;
         }
     }
