7e99cc98d1ef52a6218d009b8719e285452b83fb
Dipsw21-nep <95913036+Dipsw21-nep@users.noreply.github.com>
2022-01-03 16:03:12 +0100

    commit

--- Views/index.php
+++ Views/index.php
@@ -4,18 +4,25 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./Assets/css/style.css">
-  <title>Hello</title>
+  <title>TPLanner</title>
 </head>
+
 <body>
+
   <header>
     <?php require_once 'layout/header.php'; ?>
   </header>
 
   <main>
-    <h4>Accueil</h4>
-    <?php isset($data) ? var_dump($data) : null; ?>
+    <h1>Tasks, Team, Testing and Time Planner : TPlanner</h1>
+    <h2>La façon la plus simple de gérer vos projets</h2>
+    <p> test </p>
+    <?php //isset($data) ? var_dump($data) : null; ?>
   </main>
+
   <?php require_once 'layout/footer.php'; ?>
   <script src="./Assets/scripts/global.js"></script>
+
 </body>
+
 </html>
