7e406787eadb794e00129f9822f63d4fff954425
Roess Thomas <thomas_roess@hotmail.fr>
2022-01-06 16:02:38 +0100

    test

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
 
 echo "hello"; die();
-// Load App
-require_once 'autoloader.php';
-Autoloader::register();
 
-
-//test
-// Start Controller : NAMESPACE\CLASSNAME
-$controller = new Controllers\IndexController();
-
-// Call Controller method
-$controller->index();
-// END SCRIPT
+?>
