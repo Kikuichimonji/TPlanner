c130d66e327921cd20bb4d6d2652c32926c87872
Dipsw21-nep <95913036+Dipsw21-nep@users.noreply.github.com>
2022-01-20 11:34:17 +0100

    commit

--- Public/Views/resetPassword.php
+++ Public/Views/resetPassword.php
@@ -14,7 +14,7 @@
     </header>
 
     <main id="login">
-        <h1>FORGOT PASSWORD</h1>
+        <h1>Réinitialisez votre mot de passe TPlanner</h1>
         <p class="error">
             <?php
             if (isset($data)) {
@@ -24,13 +24,20 @@
             }
             ?>
         </p>
-        <h5>You forgot your password? We're gonna send you a new one by email.</h5>
+
+        <section class="forms_container">
+        <div class="forms">
         <form action="login.php?act=submit" method="post">
+        <fieldset class="fieldset_container">
+        <p>Vous avez oublié votre mot de passe ? Nous vous en envoyons un nouveau par mail.</p>
+        <!--You forgot your password? We're gonna send you a new one by email.-->
             <label for="mail">Email</label>
             <input type="text" name="mail" id="mail">
-            <button type="submit">Send new password</button>
+            </br></br>
+            <button type="submit">Envoyer un nouveau mot de passe</button>
             <input type="hidden" name="token" id="token" value="<?php echo $data['token']; ?>" />
             <input type="hidden" name="reset" id="reset"/>
+        </fieldset>
         </form>
     </main>
 
