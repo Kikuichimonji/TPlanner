db913ce49ba8df94b565ecaefc9aa22f3d5473bf
Roess Thomas <thomas_roess@hotmail.fr>
2022-01-25 10:31:11 +0100

    picto drag fixr

--- Public/Views/boardContent.php
+++ Public/Views/boardContent.php
@@ -47,7 +47,7 @@
         <?php
         foreach ($board->getListLists() as $list) {
             if(!$list->getIsArchiveList() && !$list->getIsArchived()){
-                echo "<div class='listContainer'><div class='listHeader' draggable='true'><span><img src='".IMG_PATH."/tplanner_picto_list1.svg' class='picto'><span class='listTitle'>".e($list->getLabel())."</span></span><span class='menu'>...</span></div>";
+                echo "<div class='listContainer'><div class='listHeader' draggable='true'><span><img draggable='false' src='".IMG_PATH."/tplanner_picto_list1.svg' class='picto'><span class='listTitle'>".e($list->getLabel())."</span></span><span class='menu'>...</span></div>";
                 echo "<ul class='list' id='{$list->getId()}'>";
                 foreach ($list->getListCards() as $card) {
                     if(!$card->getIsArchived()){
@@ -94,7 +94,7 @@
                             echo "<div class='listContainer' id={$list->getId()}>
                                     <div class='listHeader' draggable='true'>
                                         <span>
-                                            <img src='".IMG_PATH."/tplanner_picto_list1.svg' class='picto'>
+                                            <img draggable='false' src='".IMG_PATH."/tplanner_picto_list1.svg' class='picto'>
                                             <span>".e($list->getLabel())."</span>
                                         </span>";
                             if($isCreator){
