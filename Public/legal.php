5dcb8f04bf2eb1a7a35f0912da75625e2e07bd71
Roess Thomas <thomas_roess@hotmail.fr>
2022-01-26 22:03:25 +0100

    regrouped in one controller

--- Public/legal.php
+++ Public/legal.php
@@ -1,9 +1,6 @@
 <?php
 
 use Controllers\IndexController;
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 // Load App
 require_once 'autoloader.php';
