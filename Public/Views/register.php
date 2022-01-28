e9c7db5f40f2ab5e3426543cc811c3e137a1609c
Dipsw21-nep <95913036+Dipsw21-nep@users.noreply.github.com>
2022-01-28 14:34:07 +0100

    commit

--- Public/Views/register.php
+++ Public/Views/register.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= CSS_PATH ?>/style.css">
     <link rel="icon" type="image/svg" href="<?= IMG_PATH ?>/T_logo.svg" alt="favicon logo simplifié de TPlanner"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
     <title>REGISTER</title>
 </head>
 <body>
@@ -29,16 +32,16 @@
         <form action="register.php?act=submit" method="post">
         <fieldset class="fieldset_container">
             <label for="pseudo">Pseudo</label>
-            <input type="text" name="pseudo" id="pseudo" <?php if(isset($data["pseudo"])){echo "value='{$data["pseudo"]}'"; } ?>>
+            <input class="input_form" type="text" name="pseudo" id="pseudo" <?php if(isset($data["pseudo"])){echo "value='{$data["pseudo"]}'"; } ?>>
             </br>
             <label for="mail">Email</label>
-            <input type="text" name="mail" id="mail" <?php if(isset($data["mail"])){echo "value='{$data["mail"]}'";} ?>>
+            <input class="input_form" type="text" name="mail" id="mail" <?php if(isset($data["mail"])){echo "value='{$data["mail"]}'";} ?>>
             </br>
             <label for="password">Password <span>(8 charactères minimum, pas de symboles)</span></label>
-            <input type="password" name="password" id="password">
+            <input class="input_form" type="password" name="password" id="password">
             </br>
             <label for="password2">Confirmez le Password</label>
-            <input type="password" name="password2" id="password2">
+            <input class="input_form" type="password" name="password2" id="password2">
             <p>En vous inscrivant, vous confirmez avoir lu et accepté nos conditions de service et notre politique de confidentialité.</p>
             <button type="submit">S'inscrire</button>
             <input type="hidden" name="token" id="token" value="<?php echo $data['token']; ?>" />
